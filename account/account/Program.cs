1170d7888eabeb0c78cfb2bf341ac0eefc89f3af
18-38606-2@student.aiub.edu <18-38606-2@student.aiub.edu>
2021-02-07 21:46:42 +0600

    account

---
+++ account/account/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace account
+{
+
+    class account
+    {
+        string accname;
+        string acid;
+        int balance;
+        int amount;
+
+        public account()
+        {
+            Console.WriteLine("empty constructor");
+        }
+        public account(string accname, string acid, int balance, int amount)
+        {
+            Console.WriteLine("parametrized constructor");
+            this.accname = accname;
+            this.acid = acid;
+            this.balance = balance;
+            this.amount = amount;
+
+        }
+        internal void showdetails()
+        {
+            Console.WriteLine("account details ================================================");
+            Console.WriteLine("name: " + accname);
+            Console.WriteLine("id : " + acid);
+            Console.WriteLine("deposit amount: " + balance);
+            Console.WriteLine("withdraw amount: " + amount);
+            Console.WriteLine("account details ================================================");
+        }
+        static void Main(string[] args)
+        {
+            account obj = new account();
+            account obj1 = new account("mahzabin mostary", "6565764647", 578998888, 987655);
+            obj1.showdetails();
+        }
+    }
+}
