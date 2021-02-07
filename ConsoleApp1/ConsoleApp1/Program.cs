dc9e5f631f431f8cf00fc9786ede949ac82c3c94
18-38606-2@student.aiub.edu <18-38606-2@student.aiub.edu>
2021-02-07 12:59:32 +0600

    console application

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i; 
+            int n = 100;
+            int sum=0;
+            Console.WriteLine(" enter a even number",n);
+            for(i=0; i<=n; i++)
+                sum = sum / 2;
+            if (n == 2)
+            { 
+
+                Console.WriteLine(" this is even number";
+            }
+            else
+            {
+                Console.WriteLine{ " this is an odd number"}
+            }
+           
+       
+    
+
