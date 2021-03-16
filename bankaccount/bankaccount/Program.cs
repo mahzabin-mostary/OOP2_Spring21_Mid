1be674e2238db45aa3afc25140b90be62fcf09a5
18-38606-2@student.aiub.edu <18-38606-2@student.aiub.edu>
2021-03-16 12:02:35 +0600

    abst

--- bankaccount/bankaccount/Program.cs
+++ bankaccount/bankaccount/Program.cs
@@ -8,8 +8,77 @@ namespace bankaccount
 {
     class Program
     {
-        static void Main(string[] args)
+      
+
+namespace BankAccount
+    {
+         abstruct class Customer
         {
+            public string Name { get; set; }
+            Account[] accounts;
+            public int AccCount { get; set; }
+            public Customer()
+            {
+                accounts = new Account[10];
+            }
+            public void AddAccount(params Account[] accounts)
+            {
+                foreach (Account acc in accounts)
+                {
+                    this.accounts[AccCount++] = acc;
+                }
+            }
+            public void ShowAccDetails()
+            {
+                for (int i = 0; i < AccCount; i++)
+                {
+                    accounts[i].ShowInfo();
+                }
+            }
+            public Account GetAccount(string accNo)
+            {
+                Account acc = null;
+                for (int i = 0; i < AccCount; i++)
+                {
+                    if (accNo.Equals(accounts[i].AccNo))
+                    {
+                        acc = accounts[i];
+                        break;
+                    }
+                }
+                return acc;
+            }
+            abstruct class SavingsAccount : Account
+        {
+            public SavingsAccount() { }
+            public SavingsAccount(string accName, string accNo, double balance) : base(accName, accNo, balance)
+            {
+
+            }
+            public override void Withdraw(double amount)
+            {
+                if (Balance - amount >= 500)
+                {
+                    Balance -= amount;
+                    Console.WriteLine("Your acc has been debited by {0} to self", amount);
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient Balance: Min Balance 500");
+                }
+            }
+            public override void Transfer(Account acc, double amount)
+            {
+                if (Balance - amount >= 500)
+                {
+                    this.Balance -= amount;
+                    acc.Balance += amount;
+                    Console.WriteLine("Your acc has been debited by {0} to {1}({2})", amount, acc.AccName, acc.AccNo);
+                }
+            }
+
         }
+    }
+}
     }
 }
